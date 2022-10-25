dbb6f1a8258fd89b77ca99c8acede98f8cff2162
chandubomma <bommachandugoud@gmail.com>
2022-10-25 23:47:08 +0530

    Administrator.java updated

--- USER/ADMINISTRATOR/Administrator.java
+++ USER/ADMINISTRATOR/Administrator.java
@@ -1,5 +1,67 @@
 package USER.ADMINISTRATOR;
 
-public class Administrator {
-    
+import java.util.ArrayList;
+
+import USER.User;
+/*
+ * Administrator class inherits User class
+ */
+
+public class Administrator extends User{
+    private String AdministratorKey;
+
+    public Administrator(String firstName, String lastName, String email, String password, String gender,
+            String mobileNumber, String dateOfBirth, String administratorKey) {
+        super(firstName, lastName, email, password, gender, mobileNumber, dateOfBirth);
+        AdministratorKey = administratorKey;
+    }
+
+    @Override
+    public boolean Login() {
+        // TODO 
+        return false;
+    }
+
+    @Override
+    public boolean Register() {
+        // TODO
+        return false;
+    }
+
+    @Override
+    public boolean Logout() {
+        // TODO 
+        return false;
+    }
+
+    @Override
+    public boolean deleteUser() {
+        // TODO 
+        return false;
+    }
+
+    public String getAdministratorKey() {
+        return AdministratorKey;
+    }
+
+    public void setAdministratorKey(String administratorKey) {
+        AdministratorKey = administratorKey;
+    }
+    /* methods, a administrator has access to do  */
+    public ArrayList<User> getAllUserList(){
+      
+        //TODO
+        return null;
+    }
+
+    public boolean approveUser(User user){
+        //TODO
+        return false;
+    }
+    public boolean removeUser(User user){
+        //TODO
+        return false;
+    }
+
+    /*more methods to add come here */
 }
