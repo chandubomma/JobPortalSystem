e98cb0330d1a65344e457c26cca2b742bee60b98
chandubomma <bommachandugoud@gmail.com>
2022-10-28 22:23:32 +0530

    Database.java updated

--- DATABASE/Database.java
+++ DATABASE/Database.java
@@ -1,5 +1,34 @@
 package DATABASE;
 
+
 public class Database {
-    
+    	private  String dbURL="jdbc:mysql://localhost:3306/jobportalsystem";
+    	private String dbUserName="root";
+    	private String dbPassword="chandu@mysql";
+        public Database(){}
+        public Database(String dbURL, String dbUserName, String dbPassword) {
+            this.dbURL = dbURL;
+            this.dbUserName = dbUserName;
+            this.dbPassword = dbPassword;
+        }
+        public String getDbURL() {
+            return dbURL;
+        }
+        public void setDbURL(String dbURL) {
+            this.dbURL = dbURL;
+        }
+        public String getDbUserName() {
+            return dbUserName;
+        }
+        public void setDbUserName(String dbUserName) {
+            this.dbUserName = dbUserName;
+        }
+        public String getDbPassword() {
+            return dbPassword;
+        }
+        public void setDbPassword(String dbPassword) {
+            this.dbPassword = dbPassword;
+        }
+    	
 }
+
