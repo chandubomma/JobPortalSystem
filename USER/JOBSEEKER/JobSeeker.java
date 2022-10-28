a555dcc156f95f3f07c5e2cc87d586ff184dc123
Nithin2701s <115217815+Nithin2701s@users.noreply.github.com>
2022-10-28 17:59:37 +0530

    Updated JobSeeker

--- USER/JOBSEEKER/JobSeeker.java
+++ USER/JOBSEEKER/JobSeeker.java
@@ -1,5 +1,46 @@
 package USER.JOBSEEKER;
+import USER.User;
+public class JobSeeker extends User{
+   private String qualification;
 
-public class JobSeeker{
-    
+    public JobSeeker (String firstName, String lastName, String email, String password, String gender, String mobileNumber,
+    String dateOfBirth, String Qualification){
+        super(firstName, lastName, email, password, gender, mobileNumber, dateOfBirth);
+        qualification=Qualification;
+    }
+    void setQualification(String Qualification){
+      this.qualification=Qualification;
+    }
+    String getQualification(){
+       return qualification; 
+    }
+     @Override
+    public boolean Login() {
+       if(this.getEmail().equals(email)){
+         if(this.getPassword().equals(password)){
+            return true;
+         } 
+       }
+       else {
+        System.out.println("Invalid Email");
+       }
+        return false;
+    }
+    @Override
+    public boolean Register() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    @Override
+    public boolean Logout() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    @Override
+    public boolean deleteUser() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+   
 }
+
