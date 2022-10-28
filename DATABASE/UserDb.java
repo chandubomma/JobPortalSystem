c517e3a5fa2ab6e9a30876a5552dc41c059b09a4
chandubomma <bommachandugoud@gmail.com>
2022-10-28 22:24:52 +0530

    UserDb.java file added

---
+++ DATABASE/UserDb.java
@@ -0,0 +1,34 @@
+package DATABASE;
+import java.sql.*;
+
+import USER.User;
+
+public class UserDb  {
+    private static Connection connection;
+    private Database database;
+    private Statement statement;
+
+    public UserDb() {
+        database = new Database();
+        
+       try {
+        connection = DriverManager.getConnection(database.getDbURL(),database.getDbUserName(),database.getDbPassword());
+         statement = connection.createStatement();
+        } catch (SQLException e) {
+       
+            e.printStackTrace();
+        }
+    }
+
+    public  boolean addUserRecord(User user) throws SQLException{
+        String Query = "insert into user values("+user.getFirstName()+","+user.getLastName()+","+user.getEmail()+","+user.getPassword()+","+user.getGender()+","+user.getMobileNumber()+","+user.getDateOfBirth()+")";
+        return statement.execute(Query);
+    }
+     
+    public boolean deleteUserRecord(User user) throws SQLException{
+        String Query = "delete from user where email="+user.getEmail();
+        return statement.execute(Query);
+    }
+
+    // TODO methods
+}
