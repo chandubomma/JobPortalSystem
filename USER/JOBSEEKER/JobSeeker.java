222b09663f361292cbf997e6436e8eb4e3ac5733
Meghana266 <115703687+Meghana266@users.noreply.github.com>
2022-11-08 22:37:16 +0530

    Update JobSeeker.java

--- USER/JOBSEEKER/JobSeeker.java
+++ USER/JOBSEEKER/JobSeeker.java
@@ -6,9 +6,10 @@ import java.util.*;
 import USER.User;
 import USER.RECRUITER.Job;
 import MAIN.Main;
+import java.sql.*;
 
 public class JobSeeker extends User{
-    private String userKey;
+    private String jobSeekerKey;
     private int age;
     private String college;
     private String qualification;
@@ -19,14 +20,14 @@ public class JobSeeker extends User{
     private int experience;
 
     ArrayList<Job> eligibleJobs = new ArrayList<Job>();
-    ArrayList<Job> appliedJobs = new ArrayList<Job>();   
-
-    public JobSeeker(String firstName, String lastName, String email, String password, String gender, 
-                    String mobileNumber, String dateOfBirth, String userKey, String college, String qualification,
-                    int percentage, String skill1, String skill2, String skill3, int experience) 
+    ArrayList<Job> appliedJobs = new ArrayList<Job>();
+    public Connection con;
+    public Statement stmt;
+    
+    public JobSeeker(String userKey, String firstName, String lastName, String email, String password, String gender, String mobileNumber, String dateOfBirth, String college, String qualification, int percentage, String skill1, String skill2, String skill3, int experience) 
     {
         super(firstName, lastName, email, password, gender, mobileNumber, dateOfBirth);
-        this.userKey = userKey;
+        this.jobSeekerKey = userKey;
         this.college = college;
         this.qualification = qualification;
         this.percentage = percentage;
@@ -34,14 +35,16 @@ public class JobSeeker extends User{
         this.skill2 = skill2;
         this.skill3 = skill3;
         this.experience = experience;
+        this.setAge();
+        this.setEligibleJobs();  
     }
 
-    public String getUserKey() {
-        return userKey;
+    public String getJobSeekerKey() {
+        return jobSeekerKey;
     }
 
-    public void setUserKey(String userKey) {
-        this.userKey = userKey;
+    public void setJobSeekerKey(String userKey) {
+        this.jobSeekerKey = userKey;
     }
 
     public int getAge() {
@@ -53,9 +56,9 @@ public class JobSeeker extends User{
         DateTimeFormatter jkl=DateTimeFormatter.ofPattern("yyyy-MM-dd");
         String formattedDate = curDate.format(jkl);
         String n=formattedDate;
-        String n1=n.substring(6,10);
+        String n1=n.substring(0,4);
         int i=Integer.parseInt(n1);
-        String n2=this.getDateOfBirth().substring(6,10);
+        String n2=this.getDateOfBirth().substring(0,4);
         int j=Integer.parseInt(n2);
         this.age =(i-j);
     }
@@ -119,18 +122,27 @@ public class JobSeeker extends User{
     public ArrayList<Job> getEligibleJobs() {
         return eligibleJobs;
     }
-
+     
     public void setEligibleJobs() {
-        int i;
-        for(i=0;i<=numberOfJobs;i++)
-        {
-            //get details of the job from job database
-            Job obj=new Job(id, jobTitle, location, companyName, deadline, numberOfJobs, maxAge, minExperience, description)
-            if(obj.isEligible(this))
-                this.eligibleJobs.add(obj);
+        try
+        {  
+            Class.forName("com.mysql.cj.jdbc.Driver");  
+            con=DriverManager.getConnection( "jdbc:mysql://localhost:3306/jobportalsystem","root","182001");   
+            stmt=con.createStatement();  
+            String query="select * from job";
+            ResultSet rs= stmt.executeQuery(query);
+            while(rs.next())
+            {
+                Job obj = new Job(rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5),rs.getInt(6),rs.getString(7),rs.getInt(8),rs.getInt(9),rs.getString(10));
+                if(obj.isEligible(this))
+                    this.eligibleJobs.add(obj);
+            }    
+        }
+        catch(Exception e)
+        { 
+            System.out.println(e);
         }
     }
-
     public ArrayList<Job> getAppliedJobs() {
         return appliedJobs;
     }
@@ -147,6 +159,18 @@ public class JobSeeker extends User{
 
     @Override
     public boolean Register() {
+        try{    
+            Connection con=DriverManager.getConnection( "jdbc:mysql://localhost:3306/jobportalsystem","root","182001");   
+            Statement stmt=con.createStatement();  
+            String query=String.format("insert into jobSeeker values('"+this.getJobSeekerKey()+"','"+this.getFirstName()+"','"+this.getLastName()+"','"+this.getEmail()+"','"+this.getPassword()+"','"+this.getGender()+"','"+this.getMobileNumber()+"','"+this.getDateOfBirth()+"','"+this.getCollege()+"','"+this.getQualification()+"','"+this.getPercentage()+"','"+this.getSkill1()+"','"+this.getSkill2()+"','"+this.getSkill3()+"','"+this.getExperience()+"')");
+            stmt.executeUpdate(query);  
+            con.close();
+            return true;
+        }
+        catch(Exception e)
+        { 
+            System.out.println(e);
+        }
         // TODO Auto-generated method stub
         return false;
     }
@@ -160,6 +184,32 @@ public class JobSeeker extends User{
     @Override
     public boolean deleteUser() {
         // TODO Auto-generated method stub
+        return false;
+    }
+
+    public boolean applyJob(String id)
+    {
+        try
+        {
+            String query="select * from job";
+            ResultSet rs= stmt.executeQuery(query);
+            while(rs.next())
+            {
+                if(rs.getString(1).equals(id))
+                {
+                    Job obj = new Job(rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5),rs.getInt(6),rs.getString(7),rs.getInt(8),rs.getInt(9),rs.getString(10));
+                    if(obj.isEligible(this))
+                    {
+                        obj.apply(this);
+                        return true;
+                    }
+                }
+            }
+        }
+        catch(Exception e)
+        { 
+            System.out.println(e);
+        }
         return false;
     }
 }
