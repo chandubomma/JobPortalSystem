64fd57781c8986bad68476543db0f65b022ef9e9
Heshvitha <115703446+Heshvitha@users.noreply.github.com>
2022-11-13 20:57:38 +0530

    Update Recruiter.java

--- USER/RECRUITER/Recruiter.java
+++ USER/RECRUITER/Recruiter.java
@@ -1,23 +1,67 @@
 package USER.RECRUITER;
+import DATABASE.RecruiterDb;
+import USER.RECRUITER.Job;
 
 import USER.User;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+
+
+
+import MAIN.Main;
 
 public class Recruiter extends User {
-    private String compnay_name;
-    private String designation;
-
-    public Recruiter(String firstName, String lastName, String email, String password, String gender,
+    private String CompanyName;
+    private String Designation;
+    private static RecruiterDb recruiterDb = new RecruiterDb();
+    public Recruiter(String companyName,String designation,String firstName, String lastName, String email, String password, String gender,
             String mobileNumber, String dateOfBirth) {
         super(firstName, lastName, email, password, gender, mobileNumber, dateOfBirth);
        super.setUserType("recruiter");
+       this.CompanyName=companyName;
+       this.Designation=designation;
     }
      
-   
+
+    public String getCompanyName() {
+        return CompanyName;
+    }
+
+
+
+    public void setCompanyName(String companyName) {
+        this.CompanyName = companyName;
+    }
+
+
+
+    public String getDesignation() {
+        return Designation;
+    }
+
+
+
+    public void setDesignation(String designation) {
+        this.Designation = designation;
+    }
+
 
     @Override
     public boolean Register() {
-        // TODO Auto-generated method stub
-        return false;
+    
+        return(recruiterDb.addUserRecord(this) &&
+        recruiterDb.addRecruiterRecord(this));
+        
+    }
+    
+    @Override
+    public boolean Login(String email, String password) throws SQLException{
+        String userPassword = recruiterDb.getUserPassword(email);
+        if(userPassword.equals(password))return true;
+        else return false;
     }
 
     @Override
@@ -28,18 +72,8 @@ public class Recruiter extends User {
 
     @Override
     public boolean deleteUser() {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    @Override
-    public boolean Login(String email, String password) {
-        // TODO Auto-generated method stub
-        return false;
-    }
+        return(recruiterDb.deleteUserRecord(this) && recruiterDb.deleteRecruiterRecord(this));
     
-
-
-
-    
+    }
 }
+
