2384bda1dc2fbb66bb2eff08434f11a331fa0b92
Nithin2701s <115217815+Nithin2701s@users.noreply.github.com>
2022-11-13 11:35:04 +0530

    Info update

--- DATABASE/INFO/Info.java
+++ DATABASE/INFO/Info.java
@@ -1,5 +1,46 @@
 package DATABASE.INFO;
 
+import java.sql.*;
+import DATABASE.Database;
+import USER.ADMINISTRATOR.Administrator;
 public class Info {
-    
+    Database Db=new Database();
+    Connection con;
+    Statement st;
+    public void infoDb() {     
+       try {
+        con = DriverManager.getConnection(Db.getDbURL(),Db.getDbUserName(),Db.getDbPassword());
+         st = con.createStatement();
+        } catch (SQLException e) {
+       
+            e.printStackTrace();
+        }
+    }
+   public void count(Administrator user) throws SQLException{
+       
+       ResultSet rs =  st.executeQuery("select usertype,count(email) from user group by usertype;");
+      
+         int js= rs.getInt(2);
+         rs.next();
+         int rc =rs.getInt(2);
+         rs.next();
+         int ad =rs.getInt(2);
+         int total =js+rc+ad;
+         System.out.println("                   Total logins : "+total);
+         System.out.println("Jobseekers : "+js+"    Recruiters : "+rc+"Administraters : "+ad);
+   }
+  public void display_company() throws SQLException{
+   int k=1;
+   ResultSet rs = st.executeQuery("select distinct company_name from jobs;");
+   while(rs.next()){
+     System.out.println(k+"."+rs.getString(1)); 
+     k++;
+   }
+  }
+ public void display_jobs() throws SQLException{
+   int k=1;
+   ResultSet rs =st.executeQuery("Select distint job from jobs;");
+   System.out.println(k+"."+rs.getString(1));
+ }
+
 }
